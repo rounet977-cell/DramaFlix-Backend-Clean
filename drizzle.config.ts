99643baf982f65e9cb949c6e925773d60e3df6af
rounet977-cell <rounet977@gmail.com>
2026-01-29 14:08:25 +0100

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   out: "./migrations",
   schema: "./shared/schema.ts",
-  dialect: "sqlite",
+  dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "./app.db",
+    url: process.env.DATABASE_URL!,
   },
 });
